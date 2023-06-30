3202f5a5a933a7c675f14a1611078dba079fbf65
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-06-30 03:53:57 +0000

    refactored 6-final-suder.js to return the results in the specified array

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstname, lastname, filename) {
-  const results = await Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)])
-  return results;
+  const res = await Promise.allSettled([signUpUser(firstname, lastname), uploadPhoto(filename)]);
+  return res.map((result) => ({
+    status: result.status,
+    value: (result.value !== undefined) ? result.value : result.reason,
+  }));
 }
