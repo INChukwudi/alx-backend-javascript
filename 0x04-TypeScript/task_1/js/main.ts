65516e368e9bda6dba614f22ba24a494c0c259de
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-07-09 12:08:16 +0100

    added the studentInterface interface, studentConstructor interface and the studentClass class using class expressions

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -23,3 +23,31 @@ const printTeacher: printTeacherFunction = function (firstName: string, lastName
     return `${firstName[0]}. ${lastName}`;
 }
 
+interface StudentInterface {
+    firstname: string;
+    lastname: string;
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+interface StudentConstructor {
+    new (firstname: string, lastname: string): StudentInterface;
+}
+
+const StudentClass: StudentConstructor = class StudentClass implements StudentInterface {
+    firstname: string;
+    lastname: string;
+
+    constructor(firstname: string, lastname: string) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+
+    displayName(): string {
+        return this.firstname;
+    }
+}
