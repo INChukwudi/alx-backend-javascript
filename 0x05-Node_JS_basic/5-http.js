9eb01f9b83bde75586cd4a089fbcd48d7d283809
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-19 22:07:03 +0000

    refacotred the 5-http.js to await the promise call

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -10,22 +10,21 @@ if (args.length !== 1) {
 }
 const databaseFile = args[0];
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   const { pathname } = url.parse(req.url);
 
   if (pathname === '/') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello Holberton School!');
   } else if (pathname === '/students') {
-    countStudents(databaseFile)
-      .then((data) => {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end(data);
-      })
-      .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
-      });
+    try {
+      const output = await countStudents(databaseFile);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(output);
+    } catch (error) {
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end(error.message);
+    }
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('Not Found!');
