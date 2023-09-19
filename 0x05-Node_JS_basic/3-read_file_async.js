f71b8c60a0ae7212ecf87871849879112b2cca1a
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-19 20:55:24 +0000

    added task 3 solution file, 3-read_file_async.js

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter(Boolean);
+      const fieldCounts = {};
+
+      for (const line of lines) {
+        const [, , , field] = line.split(',');
+        if (field !== 'field') {
+          if (fieldCounts[field]) {
+            fieldCounts[field] += 1;
+          } else {
+            fieldCounts[field] = 1;
+          }
+        }
+      }
+
+      console.log(`Number of students: ${lines.length - 1}`);
+      for (const field in fieldCounts) {
+        if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+          const count = fieldCounts[field];
+          const students = lines
+            .filter((line) => line.endsWith(field))
+            .map((line) => line.split(',')[0]);
+
+          console.log(`Number of students in ${field}: ${count}. List: ${students.join(', ')}`);
+        }
+      }
+
+      resolve();
+    });
+  });
+}
+
+module.exports = countStudents;
