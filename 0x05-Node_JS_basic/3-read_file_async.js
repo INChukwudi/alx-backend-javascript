572ffb60f7868f03b9804ac469eb97ff904a7975
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-20 11:51:52 +0000

    refactored 3-read_file_async to log messages and also return it

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -23,6 +23,7 @@ function countStudents(path) {
       }
 
       let retVal = `Number of students: ${lines.length - 1}`;
+      console.log(`Number of students: ${lines.length - 1}`);
       for (const field in fieldCounts) {
         if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
           const count = fieldCounts[field];
@@ -31,10 +32,9 @@ function countStudents(path) {
             .map((line) => line.split(',')[0]);
 
           retVal += `\nNumber of students in ${field}: ${count}. List: ${students.join(', ')}`;
+          console.log(`Number of students in ${field}: ${count}. List: ${students.join(', ')}`);
         }
       }
-
-      console.log(retVal);
 
       resolve(retVal);
     });
