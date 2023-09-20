08fc0385727822206f7b77c04d92d31664715466
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-20 11:04:15 +0000

    added task 7 solution file, 7-http_express.js

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  console.error('Usage: node 7-http_express.js <database_file>');
+  process.exit(1);
+}
+const databaseFile = args[0];
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const output = await countStudents(databaseFile);
+    res.send(output);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port);
+
+module.exports = app;
