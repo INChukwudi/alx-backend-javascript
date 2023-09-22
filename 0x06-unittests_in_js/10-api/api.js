647e92dc145aca26eb045cadf4d470f7b08e6f1c
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-22 04:51:05 +0000

    added the /available_payments in the 10-api.js file

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -11,6 +11,16 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${cartId}`);
 });
 
+app.get('/available_payments', (req, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(paymentMethods);
+});
+
 const port = 7865;
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
