7ed517e900c91b5bbc4453279f3ed58f3505f903
Ike-Nwako Chukwudi <ikenwakochukwudi@gmail.com>
2023-09-22 04:54:37 +0000

    added the /login endpoint in the 10-api.js file

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -21,6 +21,15 @@ app.get('/available_payments', (req, res) => {
   res.json(paymentMethods);
 });
 
+app.post('/login', (req, res) => {
+  const userName = req.body.username;
+  if (!userName) {
+    return res.status(400).json({ error: 'username is not found' });
+  }
+
+  res.json({ message: `Welcome ${userName}` });
+});
+
 const port = 7865;
 app.listen(port, () => {
   console.log(`API available on localhost port ${port}`);
